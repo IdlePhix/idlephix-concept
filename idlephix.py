fa28b1d27821deba54d9e26cdea3059317801bcf
phixyn <phixyn@gmail.com>
2018-08-19 22:33:10 +0100

    feat(idlephix): implement woodcutting and wood item

---
+++ idlephix.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+"""Concept work for IdlePhix.
+
+Increments things (?).
+"""
+
+
+__author__ = "Phixyn"
+__version__ = "v0.0.1"
+
+
+import os
+import subprocess
+import sys
+import time
+from util.logger import logger
+
+
+# Item and skills
+wood = 0
+woodMultiplier = 1 # TODO depends on axe/hatchet item (part of item configuration)
+
+woodcuttingLevel = 1
+woodcuttingExp = 0
+woodcuttingExpMultiplier = 0.5 # TODO should be per item (part of item configuration)
+
+
+def clear_screen():
+    """
+    Clear console and buffer
+    """
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        subprocess.call(["printf", "'\033c'"]) # https://stackoverflow.com/a/23075152
+
+
+# def increment_resources():
+#     """
+#     Increments the quantity of each item.
+#     """
+#     wood += woodMultiplier
+# 
+# 
+# def increment_exp():
+#     """
+#     Increments experience points of each skill.
+#     """
+#     woodcuttingExp += woodcuttingExpMultiplier
+
+
+if __name__ == "__main__":
+    try:
+        while True:
+            clear_screen()
+            
+            # "Update()"
+            # increment_resources()
+            # increment_exp()
+
+            # "Draw()"
+            print("\nWelcome to IdlePhix.\n")
+            logger.info("Wood: {0}".format(wood))
+            logger.info("Woodcutting exp: {0}".format(woodcuttingExp))
+            logger.info("Woodcutting level: {0}".format(woodcuttingLevel))
+            wood += woodMultiplier
+            woodcuttingExp += woodcuttingExpMultiplier
+
+            time.sleep(1) # tfw not using delta
+    except KeyboardInterrupt as ex:
+        logger.info("Exiting.")
+        sys.exit(0)
