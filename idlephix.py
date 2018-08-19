8eefe5670dd911eab409f744b1547d9a60644ef8
phixyn <phixyn@gmail.com>
2018-08-19 22:49:54 +0100

    refactor(idlephix): implement a Game class and use it in the main loop

--- idlephix.py
+++ idlephix.py
@@ -15,45 +15,55 @@ from util import console
 from util.logger import logger
 
 
-# Item and skills
-wood = 0
-woodMultiplier = 1 # TODO depends on axe/hatchet item (part of item configuration)
+class Game:
+    """
+    The Game class.
+    """
+    def __init__(self):
+        """
+        Default constructor.
+        """
+        # Item and skills
+        self.wood = 0
+        self.woodMultiplier = 1 # TODO depends on axe/hatchet item (part of item configuration)
 
-woodcuttingLevel = 1
-woodcuttingExp = 0
-woodcuttingExpMultiplier = 0.5 # TODO should be per item (part of item configuration)
+        self.woodcuttingLevel = 1
+        self.woodcuttingExp = 0
+        self.woodcuttingExpMultiplier = 0.5 # TODO should be per item (part of item configuration)
 
+    def increment_resources(self):
+        """
+        Increments the quantity of each item.
+        """
+        self.wood += self.woodMultiplier
 
-# def increment_resources():
-#     """
-#     Increments the quantity of each item.
-#     """
-#     wood += woodMultiplier
-# 
-# 
-# def increment_exp():
-#     """
-#     Increments experience points of each skill.
-#     """
-#     woodcuttingExp += woodcuttingExpMultiplier
+    def increment_exp(self):
+        """
+        Increments experience points of each skill.
+        """
+        self.woodcuttingExp += self.woodcuttingExpMultiplier
+
+    def update(self):
+        """
+        Updates the game.
+        """
+        self.increment_resources()
+        self.increment_exp()
 
 
 if __name__ == "__main__":
+    game = Game()
+
     try:
         while True:
             console.clear_screen()
-            
-            # "Update()"
-            # increment_resources()
-            # increment_exp()
+            game.update()
 
             # "Draw()"
             print("\nWelcome to IdlePhix.\n")
-            logger.info("Wood: {0}".format(wood))
-            logger.info("Woodcutting exp: {0}".format(woodcuttingExp))
-            logger.info("Woodcutting level: {0}".format(woodcuttingLevel))
-            wood += woodMultiplier
-            woodcuttingExp += woodcuttingExpMultiplier
+            logger.info("Wood: {0}".format(game.wood))
+            logger.info("Woodcutting exp: {0}".format(game.woodcuttingExp))
+            logger.info("Woodcutting level: {0}".format(game.woodcuttingLevel))
 
             time.sleep(1) # tfw not using delta
     except KeyboardInterrupt as ex:
