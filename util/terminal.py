e86d1f1167e6a55329bc8d3d3c86ffc6a69866c7
phixyn <phixyn@gmail.com>
2018-08-20 00:08:23 +0100

    refactor(terminal): rename console.py to terminal.py and add constants for text formatting escape codes

---
+++ util/terminal.py
@@ -0,0 +1,28 @@
+"""
+Contains helper methods for different terminals and shells in different platforms.
+"""
+
+
+__author__ = "Phixyn"
+
+
+import os
+import subprocess
+
+
+# ANSI escape sequences - Select Graphic Rendition subset. Allows formatting text in text terminals.
+# All of these have the form "\033[XXXm" where XXX is a series of semicolon-separated parameters.
+# More information: https://en.wikipedia.org/wiki/ANSI_escape_code
+TEXT_COLOR_MAGENTA = "\033[38;35m"
+TEXT_COLOR_MAGENTA_BOLD = "\033[38;35;1m"
+TEXT_FORMATTING_RESET = "\033[0m" # Resets the text formatting back to default
+
+
+def clear_screen():
+    """
+    Clear console and buffer
+    """
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        subprocess.call(["printf", "'\033c'"]) # https://stackoverflow.com/a/23075152
