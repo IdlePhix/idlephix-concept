33bff7f2dba2ffa459fb0f1c855e0d663d85125b
phixyn <phixyn@gmail.com>
2018-08-20 01:20:59 +0100

    refactor(game): update Game class to use Woodcutting and Wood classes

--- game.py
+++ game.py
@@ -7,6 +7,8 @@ __author__ = "Phixyn"
 
 
 from chat import Chat
+from items.wood import Wood
+from skills.woodcutting import Woodcutting
 from util import terminal
 # from util.logger import logger
 
@@ -17,45 +19,42 @@ class Game:
     """
     def __init__(self):
         """
-        Default constructor.
+        Class constructor method.
         """
         # Chat "window" area
-        self._chat = Chat(max_messages=7)
+        self._chat = Chat(max_messages=10)
         self._chat.add_message("Welcome to {textColorMagenta}IdlePhix{textFormatReset}.".
             format(textColorMagenta=terminal.TEXT_COLOR_MAGENTA, textFormatReset=terminal.TEXT_FORMATTING_RESET))
 
-        # Item and skills
-        self.wood = 0
-        self.woodMultiplier = 1 # TODO depends on axe/hatchet item (part of item configuration)
+        # Skills and items
+        self.woodcutting = Woodcutting()
+        self.wood = Wood()
 
-        self.woodcuttingLevel = 1
-        self.woodcuttingExp = 0
-        self.woodcuttingExpMultiplier = 0.5 # TODO should be per item (part of item configuration)
+    def _increment_resources_and_exp(self):
+        """
+        Increments the amount of each item and gives experience points to the associated item's skill.
+        """
+        self.wood.chop()
+        self._chat.add_message(self.wood.gatherMessage)
 
-    def _increment_resources(self):
-        """
-        Increments the quantity of each item.
-        """
-        self._chat.add_message("You chop down some {textColorMagentaBold}wood{textFormatReset}.".
-            format(textColorMagentaBold=terminal.TEXT_COLOR_MAGENTA_BOLD, textFormatReset=terminal.TEXT_FORMATTING_RESET))
-        self.wood += self.woodMultiplier
-
-    def _increment_exp(self):
-        """
-        Increments experience points of each skill.
-        """
-        self.woodcuttingExp += self.woodcuttingExpMultiplier
+        # For now, add_exp returns true if there was a level up
+        if self.woodcutting.add_exp(self.wood.woodcuttingExpMultiplier):
+            # TODO using .format here is a bit bad e_e
+            self._chat.add_message(self.woodcutting.levelUpMessage.
+                format(textColorMagentaBold=terminal.TEXT_COLOR_MAGENTA_BOLD,
+                    textFormatReset=terminal.TEXT_FORMATTING_RESET,
+                    woodcuttingLevel=self.woodcutting.level))
 
     def update(self):
         """
         Updates the game.
         """
-        self._increment_resources()
-        self._increment_exp()
+        self._increment_resources_and_exp()
 
     def draw(self):
         """
         "Draws" the game "window".
         """
-        print("Wood: {0} | Woodcutting exp: {1} | Woodcutting level: {2}".format(self.wood, self.woodcuttingExp, self.woodcuttingLevel))
+        print("Wood: {0} | Woodcutting exp: {1} | Woodcutting level: {2}".format(self.wood.amount, self.woodcutting.totalExperience, self.woodcutting.level))
+        # print("Woodcutting exp (level): {0}".format(self.woodcutting.levelExperience)) # For debugging only
         self._chat.print_chat()
